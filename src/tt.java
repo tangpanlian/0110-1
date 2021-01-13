ab3b9876740d56d9069b66f8e7137df8becfb0d5
Administrator <2734522319@qq.com>
2021-01-13 16:44:50 +0800

    2021年1月10日大数据培训自己敲的代码

---
+++ src/tt.java
@@ -0,0 +1,11 @@
+public class tt {
+    public static void main(String[] args) {
+       int a=2;
+       f(a);
+        System.out.println("RR:"+a);
+    }
+    public static void f(int a){
+        a=8;
+        System.out.println("MM:"+a);
+    }
+}
